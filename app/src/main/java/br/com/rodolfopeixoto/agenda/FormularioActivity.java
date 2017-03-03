bf35e6fbf6fa390ed0757991c3bb945d310c123b
Rodolfo Peixoto <rodolfog.peixoto@gmail.com>
2017-03-03 16:01:31 -0300

    Created form and add event in button

---
+++ app/src/main/java/br/com/rodolfopeixoto/agenda/FormularioActivity.java
@@ -0,0 +1,25 @@
+package br.com.rodolfopeixoto.agenda;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
+
+public class FormularioActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_formulario);
+
+        Button buttonSalvar = (Button) findViewById(R.id.form_salvar);
+        buttonSalvar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Toast.makeText(FormularioActivity.this, "Botao clicado!", Toast.LENGTH_SHORT).show();
+            }
+        });
+
+    }
+}
