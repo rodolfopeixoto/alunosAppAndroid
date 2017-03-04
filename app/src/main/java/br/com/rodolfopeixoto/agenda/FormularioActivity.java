c6522c35538fc8e88163273794d7d8c2e83afb92
Rodolfo Peixoto <rodolfog.peixoto@gmail.com>
2017-03-03 22:07:07 -0300

    Added save icon in action bar

--- app/src/main/java/br/com/rodolfopeixoto/agenda/FormularioActivity.java
+++ app/src/main/java/br/com/rodolfopeixoto/agenda/FormularioActivity.java
@@ -2,6 +2,9 @@ package br.com.rodolfopeixoto.agenda;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
 import android.widget.Toast;
@@ -13,14 +16,29 @@ public class FormularioActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_formulario);
 
-        Button buttonSalvar = (Button) findViewById(R.id.form_salvar);
-        buttonSalvar.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater inflater = getMenuInflater();
+
+        inflater.inflate(R.menu.menu_formulario, menu);
+
+        return super.onCreateOptionsMenu(menu);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+
+        switch (item.getItemId()){
+
+            case R.id.menu_formulario_ok:
+
                 Toast.makeText(FormularioActivity.this, "Aluno salvo.", Toast.LENGTH_SHORT).show();
                 finish();
-            }
-        });
+                break;
+        }
 
+        return super.onOptionsItemSelected(item);
     }
 }
