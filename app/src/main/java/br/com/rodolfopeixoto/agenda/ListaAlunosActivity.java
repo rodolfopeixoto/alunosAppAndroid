1bfb8bcf15c2b6d95b9f0a0c7910819b2e24bfbf
Rodolfo Peixoto <rodolfog.peixoto@gmail.com>
2017-03-23 00:38:27 -0300

    Added xml image and button

--- app/src/main/java/br/com/rodolfopeixoto/agenda/ListaAlunosActivity.java
+++ app/src/main/java/br/com/rodolfopeixoto/agenda/ListaAlunosActivity.java
@@ -1,11 +1,6 @@
 package br.com.rodolfopeixoto.agenda;
 
-import android.Manifest;
 import android.content.Intent;
-import android.content.pm.PackageManager;
-import android.net.Uri;
-import android.provider.Browser;
-import android.support.v4.app.ActivityCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.ContextMenu;
@@ -18,10 +13,6 @@ import android.widget.Button;
 import android.widget.ListView;
 import android.widget.Toast;
 
-import com.google.android.gms.appindexing.Action;
-import com.google.android.gms.appindexing.AppIndex;
-import com.google.android.gms.common.api.GoogleApiClient;
-
 import java.util.List;
 
 import br.com.rodolfopeixoto.agenda.dao.AlunoDAO;
@@ -30,11 +21,6 @@ import br.com.rodolfopeixoto.agenda.modelo.Aluno;
 public class ListaAlunosActivity extends AppCompatActivity {
 
     private ListView listaAlunos;
-    /**
-     * ATTENTION: This was auto-generated to implement the App Indexing API.
-     * See https://g.co/AppIndexing/AndroidStudio for more information.
-     */
-    private GoogleApiClient client;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -66,13 +52,10 @@ public class ListaAlunosActivity extends AppCompatActivity {
 
 
         registerForContextMenu(listaAlunos);
-        // ATTENTION: This was auto-generated to implement the App Indexing API.
-        // See https://g.co/AppIndexing/AndroidStudio for more information.
-        client = new GoogleApiClient.Builder(this).addApi(AppIndex.API).build();
     }
 
     private void carregaLista() {
-        AlunoDAO dao = new AlunoDAO(this);
+        AlunoDAO dao =  new AlunoDAO(this);
         List<Aluno> alunos = dao.buscaAlunos();
         dao.close();
 
@@ -88,113 +71,24 @@ public class ListaAlunosActivity extends AppCompatActivity {
 
     @Override
     public void onCreateContextMenu(ContextMenu menu, View v, final ContextMenu.ContextMenuInfo menuInfo) {
-
-        AdapterView.AdapterContextMenuInfo info = (AdapterView.AdapterContextMenuInfo) menuInfo;
-        final Aluno aluno = (Aluno) listaAlunos.getItemAtPosition(info.position);
-
-
-        MenuItem itemLigar = menu.add("Ligar");
-        MenuItem itemSite = menu.add("Visualizar site");
-        MenuItem itemSMS = menu.add("Enviar SMS");
-        MenuItem itemMAP = menu.add("Visualizar no mapa");
         MenuItem deletar = menu.add("Deletar");
 
-        Intent intentSMS = new Intent(Intent.ACTION_VIEW);
-        Intent intentSite = new Intent(Intent.ACTION_VIEW);
-        Intent intentMAP = new Intent(Intent.ACTION_VIEW);
-
-        // Actions
-
-        itemLigar.setOnMenuItemClickListener(new MenuItem.OnMenuItemClickListener() {
+        deletar.setOnMenuItemClickListener(new MenuItem.OnMenuItemClickListener(){
             @Override
-            public boolean onMenuItemClick(MenuItem menuItem) {
+            public boolean onMenuItemClick(MenuItem item){
 
-                if(ActivityCompat.checkSelfPermission((ListaAlunosActivity.this, Manifest.permission.CALL_PHONE) != PackageManager.PERMISSION_GRANTED ){
-
-                    ActivityCompat.requestPermissions(ListaAlunosActivity.this, new String[] {Manifest.permission.CALL_PHONE}, 123);
-
-
-                }else {
-
-                    Intent intentLigar = new Intent(Intent.ACTION_CALL);
-                    intentLigar.setData(Uri.parse("tel:" + aluno.getTelefone()));
-                    startActivity(intentLigar);
-                }
-
-
-
-                return false;
-            }
-        });
-
-        String site = aluno.getSite();
-
-        if (!site.startsWith("http://")) {
-            site = "http://" + site;
-        }
-
-        intentSite.setData(Uri.parse(site));
-        itemSite.setIntent(intentSite);
-
-        intentSMS.setData(Uri.parse("sms:" + aluno.getTelefone()));
-        itemSMS.setIntent(intentSMS);
-
-        intentMAP.setData(Uri.parse("geo:0,0?q=" + aluno.getEndereco()));
-        itemMAP.setIntent(intentMAP);
-
-        deletar.setOnMenuItemClickListener(new MenuItem.OnMenuItemClickListener() {
-            @Override
-            public boolean onMenuItemClick(MenuItem item) {
+                AdapterView.AdapterContextMenuInfo info = (AdapterView.AdapterContextMenuInfo) menuInfo;
+                Aluno aluno = (Aluno) listaAlunos.getItemAtPosition(info.position);
 
                 AlunoDAO dao = new AlunoDAO(ListaAlunosActivity.this);
                 dao.deleta(aluno);
 
                 carregaLista();
 
-                Toast.makeText(ListaAlunosActivity.this, "Deletar o aluno " + aluno.getNome(), Toast.LENGTH_SHORT).show();
+                Toast.makeText(ListaAlunosActivity.this, "Deletar o aluno " + aluno.getNome(), Toast.LENGTH_SHORT ).show();
                 return false;
             }
         });
 
-    }
-
-    @Override
-    public void onStart() {
-        super.onStart();
-
-        // ATTENTION: This was auto-generated to implement the App Indexing API.
-        // See https://g.co/AppIndexing/AndroidStudio for more information.
-        client.connect();
-        Action viewAction = Action.newAction(
-                Action.TYPE_VIEW, // TODO: choose an action type.
-                "ListaAlunos Page", // TODO: Define a title for the content shown.
-                // TODO: If you have web page content that matches this app activity's content,
-                // make sure this auto-generated web page URL is correct.
-                // Otherwise, set the URL to null.
-                Uri.parse("http://host/path"),
-                // TODO: Make sure this auto-generated app URL is correct.
-                Uri.parse("android-app://br.com.rodolfopeixoto.agenda/http/host/path")
-        );
-        AppIndex.AppIndexApi.start(client, viewAction);
-    }
-
-    @Override
-    public void onStop() {
-        super.onStop();
-
-        // ATTENTION: This was auto-generated to implement the App Indexing API.
-        // See https://g.co/AppIndexing/AndroidStudio for more information.
-        Action viewAction = Action.newAction(
-                Action.TYPE_VIEW, // TODO: choose an action type.
-                "ListaAlunos Page", // TODO: Define a title for the content shown.
-                // TODO: If you have web page content that matches this app activity's content,
-                // make sure this auto-generated web page URL is correct.
-                // Otherwise, set the URL to null.
-                Uri.parse("http://host/path"),
-                // TODO: Make sure this auto-generated app URL is correct.
-                Uri.parse("android-app://br.com.rodolfopeixoto.agenda/http/host/path")
-        );
-        AppIndex.AppIndexApi.end(client, viewAction);
-        client.disconnect();
     }
 }
