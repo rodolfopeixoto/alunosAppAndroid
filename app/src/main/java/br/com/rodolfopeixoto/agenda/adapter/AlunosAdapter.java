ae6a76f6d65669229ffba22179482bc704f67c52
Rodolfo Peixoto <rodolfog.peixoto@gmail.com>
2017-03-27 23:54:06 -0300

    Better UX and performance

--- app/src/main/java/br/com/rodolfopeixoto/agenda/adapter/AlunosAdapter.java
+++ app/src/main/java/br/com/rodolfopeixoto/agenda/adapter/AlunosAdapter.java
@@ -1,13 +1,18 @@
 package br.com.rodolfopeixoto.agenda.adapter;
 
 import android.content.Context;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.BaseAdapter;
+import android.widget.ImageView;
 import android.widget.TextView;
 
 import java.util.List;
 
+import br.com.rodolfopeixoto.agenda.R;
 import br.com.rodolfopeixoto.agenda.modelo.Aluno;
 
 /**
@@ -39,11 +44,34 @@ public class AlunosAdapter extends BaseAdapter {
     }
 
     @Override
-    public View getView(int position, View convertView, ViewGroup viewGroup) {
+    public View getView(int position, View convertView, ViewGroup parent) {
 
-        TextView view = new TextView(context);
+
         Aluno aluno = alunos.get(position);
-        view.setText(aluno.toString());
+
+        LayoutInflater layoutInflater = LayoutInflater.from(context);
+
+        View view = convertView;
+
+        if(view == null){
+            view = layoutInflater.inflate(R.layout.list_item, parent, false);
+        }
+
+        TextView campoNome = (TextView) view.findViewById(R.id.item_nome);
+        campoNome.setText(aluno.getNome());
+
+        TextView campoTelefone = (TextView) view.findViewById(R.id.item_telefone);
+        campoTelefone.setText(aluno.getTelefone());
+
+        ImageView campoFoto = (ImageView) view.findViewById(R.id.item_foto);
+
+        String caminhoFoto = aluno.getCaminhoFoto();
+        if(caminhoFoto != null){
+            Bitmap bitmap = BitmapFactory.decodeFile(caminhoFoto);
+            Bitmap bitmapReduzido = Bitmap.createScaledBitmap(bitmap, 100, 100, true);
+            campoFoto.setImageBitmap(bitmapReduzido);
+            campoFoto.setScaleType(ImageView.ScaleType.FIT_XY);
+        }
         return view;
     }
 }
