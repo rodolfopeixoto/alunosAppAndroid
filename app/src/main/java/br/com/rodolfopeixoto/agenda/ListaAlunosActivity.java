ea8ef6a90e5dbb4e36366ce4a96dc27470514519
Rodolfo Peixoto <rodolfog.peixoto@gmail.com>
2017-04-04 15:37:25 -0300

    Created broadcast receive SMS

--- app/src/main/java/br/com/rodolfopeixoto/agenda/ListaAlunosActivity.java
+++ app/src/main/java/br/com/rodolfopeixoto/agenda/ListaAlunosActivity.java
@@ -1,6 +1,9 @@
 package br.com.rodolfopeixoto.agenda;
 
+import android.Manifest;
 import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.support.v4.app.ActivityCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.ContextMenu;
@@ -27,6 +30,11 @@ public class ListaAlunosActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_lista_alunos);
+
+
+        if(ActivityCompat.checkSelfPermission(ListaAlunosActivity.this, Manifest.permission.RECEIVE_SMS) != PackageManager.PERMISSION_GRANTED){
+            ActivityCompat.requestPermissions(ListaAlunosActivity.this, new String[] { Manifest.permission.RECEIVE_SMS}, 2);
+        }
 
 
         listaAlunos = (ListView) findViewById(R.id.lista_alunos);
---
+++ app/src/main/java/br/com/rodolfopeixoto/agenda/receiver/SMSReceiver.java
@@ -0,0 +1,20 @@
+package br.com.rodolfopeixoto.agenda.receiver;
+
+import android.Manifest;
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.support.v4.app.ActivityCompat;
+import android.widget.Toast;
+
+/**
+ * Created by rodolfopeixoto on 04/04/17.
+ */
+
+public class SMSReceiver extends BroadcastReceiver {
+    @Override
+    public void onReceive(Context context, Intent intent) {
+        Toast.makeText(context, "Chegou uma SMS!", Toast.LENGTH_LONG).show();
+    }
+}
