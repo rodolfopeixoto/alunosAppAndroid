1eb732c0d4370572d7e2e714eab59afc1319eb21
Rodolfo Peixoto <rodolfog.peixoto@gmail.com>
2017-04-08 16:14:39 -0300

    Finished fragments

--- app/src/main/java/br/com/rodolfopeixoto/agenda/ProvasActivity.java
+++ app/src/main/java/br/com/rodolfopeixoto/agenda/ProvasActivity.java
@@ -1,18 +1,10 @@
 package br.com.rodolfopeixoto.agenda;
 
-import android.content.Intent;
+
 import android.support.v4.app.FragmentManager;
 import android.support.v4.app.FragmentTransaction;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.view.View;
-import android.widget.AdapterView;
-import android.widget.ArrayAdapter;
-import android.widget.ListView;
-import android.widget.Toast;
-
-import java.util.Arrays;
-import java.util.List;
 
 import br.com.rodolfopeixoto.agenda.modelo.Prova;
 
@@ -36,5 +28,28 @@ public class ProvasActivity extends AppCompatActivity {
 
     private boolean estaNoModoPaisagem() {
         return getResources().getBoolean(R.bool.modoPaisagem);
+    }
+
+    public void selecionaProva(Prova prova) {
+
+        FragmentManager manager = getSupportFragmentManager();
+        if(!estaNoModoPaisagem()) {
+            FragmentTransaction tx = manager.beginTransaction();
+            DetalhesProvaFragment detalhesProvaFragment = new DetalhesProvaFragment();
+
+            Bundle parametros = new Bundle();
+            parametros.putSerializable("prova",prova);
+
+            detalhesProvaFragment.setArguments(parametros);
+            tx.replace(R.id.frame_principal, detalhesProvaFragment);
+            tx.addToBackStack(null);
+            tx.commit();
+        }else{
+            DetalhesProvaFragment detalhesFragment = (DetalhesProvaFragment) manager.findFragmentById(R.id.frame_secundario);
+            detalhesFragment.populaCamposCom(prova);
+
+        }
+
+
     }
 }
